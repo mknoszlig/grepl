9d5e28daf69f8a5d849c5fcebeb4115e0ef81426
Maximilian Karasz <maximilian.karasz@googlemail.com>
2014-01-10 10:51:13 +0100

    first iteration of recursive parser.
    problem: will tokenize at \newline and \space but
    can't distinguish later on...

--- src/grepl/core.clj
+++ src/grepl/core.clj
@@ -1,19 +1,65 @@
 (ns grepl.core)
 
-(def str "module Demo {
+(def s "module Demo {
     interface Printer {
         void printString(string s);
     };
+};
+
+module Foo {
 };")
 
-(defn detect-module [str]
-  (let [re #"(?s)module\s+(\S+)\s*\{(.*)$"]
-    (re-find re str)))
+(defn update-brackets [m c]
+  (cond (= c \{) (update-in m [:curly] inc)
+        (= c \}) (update-in m [:curly] dec)
+        (= c \() (update-in m [:round] inc)
+        (= c \)) (update-in m [:round] dec)
+        (= c \[) (update-in m [:square] inc)
+        (= c \]) (update-in m [:square] dec)
+        :else m))
+
+(def spacey? #{\space \newline})
+
+(defn tokenize-level
+  ([s]
+     (tokenize-level s 0))
+  ([s level]
+     (loop [global-acc []
+            {:keys [curly square round] :as bracket-level} {:curly 0
+                                                            :square 0
+                                                            :round 0}
+            word-acc []
+            [h & t] s]
+       (cond (nil? h) (conj global-acc (apply str word-acc))
+             (or (> curly level)
+                 (not (spacey? h))) (recur global-acc
+                                         (update-brackets bracket-level h)
+                                         (conj word-acc h)
+                                         t)
+                 (spacey? h) (recur (conj global-acc (apply str word-acc))
+                                    (update-brackets bracket-level h)
+                                     []
+                                     t)))))
+
+(defmulti parse-keyword (fn [tokens level] (first tokens)))
+
+(defmethod parse-keyword "module" [[_ name contents & more] level]
+  (cons 
+   {:module name
+    :contents (parse-keyword (tokenize-level contents (inc level)) (inc level))}
+   (parse-keyword more level)))
+
+(defmethod parse-keyword "interface" [[_ name contents & more] level]
+  (cons 
+   {:interface name
+    :contents (parse-keyword (tokenize-level contents (inc level)) (inc level))}
+   (parse-keyword more level)))
+
+(defmethod parse-keyword :default [[t & tokens] level]
+  (if t
+    (vec (cons t (parse-keyword tokens level)))
+    []))
 
 
-(defn walk [[h & t] acc]
-  (cond (= \{ h) (let [[step new-t] (walk t [])]
-                   (walk new-t (conj acc step)))
-        (= \} h) [acc t]
-        (nil? h) acc
-        :else (walk t (conj acc h))))
+;; example usage:
+;; (-> s (tokenize-level) (parse-keyword 0))
